ca2bb0cb33b5fc83e2327cd77fd5dccb3c8cb07b
Stephen Hussey <shussey@us.ibm.com>
2017-12-19 14:22:57 -0800

    mysql

---
+++ bootstrap-mysql.sh
@@ -0,0 +1,30 @@
+
+
+# DB_USER_NAME=<admin>
+# DB_PWD=<SADFZCZVXZVC>
+# DB_NAME=<compose>
+# DB_CXN_URL=<jdbc:mysql://bluemix-sandbox-dal-9-portal.6.dblayer.com:12121?createDatabaseIfNotExist=true>
+
+# assumes "script_params": ["DB_USER_NAME", "DB_PWD", "DB_NAME", "DB_CXN_URL-HOST" , "DB_CXN_URL-PORT"]
+
+DB_USER_NAME=$1
+DB_PWD=$2
+DB_NAME=$3
+DB_CXN_URL="jdbc:mysql://"$4":"$5"?createDatabaseIfNotExist=true"
+
+if [ "x$NODE_TYPE" == "xmanagement" ]
+then
+
+    echo "Node type is xmanagement hence updating ambari properties"
+    /var/lib/ambari-server/resources/scripts/configs.sh -u $AMBARI_USER -p $AMBARI_PASSWORD -port $AMBARI_PORT -s set $AMBARI_HOST  $CLUSTER_NAME hive-site "javax.jdo.option.ConnectionURL" $DB_CXN_URL /var/lib/ambari-server/resources/scripts/configs
+    /var/lib/ambari-server/resources/scripts/configs.sh -u $AMBARI_USER -p $AMBARI_PASSWORD -port $AMBARI_PORT -s set $AMBARI_HOST  $CLUSTER_NAME hive-site "javax.jdo.option.ConnectionUserName" $DB_USER_NAME
+    /var/lib/ambari-server/resources/scripts/configs.sh -u $AMBARI_USER -p $AMBARI_PASSWORD -port $AMBARI_PORT -s set $AMBARI_HOST  $CLUSTER_NAME hive-site "javax.jdo.option.ConnectionPassword" $DB_PWD
+    /var/lib/ambari-server/resources/scripts/configs.sh -u $AMBARI_USER -p $AMBARI_PASSWORD -port $AMBARI_PORT -s set $AMBARI_HOST $CLUSTER_NAME hive-site "ambari.hive.db.schema.name" $DB_NAME
+
+    echo "stop and Start Services"
+    curl -v --user $AMBARI_USER:$AMBARI_PASSWORD -H "X-Requested-By: ambari" -i -X PUT -d '{"RequestInfo": {"context": "Stop All Services via REST"}, "ServiceInfo": {"state":"INSTALLED"}}' https://$AMBARI_HOST:$AMBARI_PORT/api/v1/clusters/$CLUSTER_NAME/services
+    sleep 100
+
+    curl -v --user $AMBARI_USER:$AMBARI_PASSWORD -H "X-Requested-By: ambari" -i -X PUT -d '{"RequestInfo": {"context": "Start All Services via REST"}, "ServiceInfo": {"state":"STARTED"}}' https://$AMBARI_HOST:$AMBARI_PORT/api/v1/clusters/$CLUSTER_NAME/services
+    sleep 700
+fi
