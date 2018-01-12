82299c0e6fcee54b9280ce71438dba592f1fba19
Stephen Hussey <shussey@us.ibm.com>
2018-01-12 09:21:41 -0800

    mysql updates

--- bootstrap-mysql.sh
+++ bootstrap-mysql.sh
@@ -18,14 +18,24 @@ echo "MySQL URL: "$DB_CXN_URL
 
 echo "NODE_TYPE: "$NODE_TYPE1
 
+# NODE_TYPE options are data, management-slave1, or manangement-slave2
+
 # if [ "x$NODE_TYPE" == "xmanagement" ]
 if [ "x$NODE_TYPE" == "xmanagement-slave2" ]
 then
 
-    echo "Node type is xmaster-management hence updating ambari properties"
-    /var/lib/ambari-server/resources/scripts/configs.sh -u $AMBARI_USER -p $AMBARI_PASSWORD -port $AMBARI_PORT -s set $AMBARI_HOST  $CLUSTER_NAME hive-site "javax.jdo.option.ConnectionURL" $DB_CXN_URL /var/lib/ambari-server/resources/scripts/configs
+    echo "Node type is xmanagement-slave2 hence updating ambari properties"
+    
+    echo "javax.jdo.option.ConnectionURL = $DB_CXN_URL"
+    /var/lib/ambari-server/resources/scripts/configs.sh -u $AMBARI_USER -p $AMBARI_PASSWORD -port $AMBARI_PORT -s set $AMBARI_HOST  $CLUSTER_NAME hive-site "javax.jdo.option.ConnectionURL" $DB_CXN_URL
+
+    echo "javax.jdo.option.ConnectionUserName = $DB_USER_NAME"
     /var/lib/ambari-server/resources/scripts/configs.sh -u $AMBARI_USER -p $AMBARI_PASSWORD -port $AMBARI_PORT -s set $AMBARI_HOST  $CLUSTER_NAME hive-site "javax.jdo.option.ConnectionUserName" $DB_USER_NAME
+
+    echo "javax.jdo.option.ConnectionPassword = $DB_PWD"
     /var/lib/ambari-server/resources/scripts/configs.sh -u $AMBARI_USER -p $AMBARI_PASSWORD -port $AMBARI_PORT -s set $AMBARI_HOST  $CLUSTER_NAME hive-site "javax.jdo.option.ConnectionPassword" $DB_PWD
+
+    echo "ambari.hive.db.schema.name = $DB_NAME"
     /var/lib/ambari-server/resources/scripts/configs.sh -u $AMBARI_USER -p $AMBARI_PASSWORD -port $AMBARI_PORT -s set $AMBARI_HOST $CLUSTER_NAME hive-site "ambari.hive.db.schema.name" $DB_NAME
 
     echo "stop and Start Services"
@@ -34,4 +44,6 @@ then
 
     curl -v --user $AMBARI_USER:$AMBARI_PASSWORD -H "X-Requested-By: ambari" -i -X PUT -d '{"RequestInfo": {"context": "Start All Services via REST"}, "ServiceInfo": {"state":"STARTED"}}' https://$AMBARI_HOST:$AMBARI_PORT/api/v1/clusters/$CLUSTER_NAME/services
     sleep 700
+    
+    echo "Completed customization"
 fi
