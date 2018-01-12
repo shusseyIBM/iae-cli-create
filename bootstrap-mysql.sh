48c2d24266548f8752c4c56a31410704735ab034
Stephen Hussey <shussey@us.ibm.com>
2018-01-12 10:14:28 -0800

    mysql fix

--- bootstrap-mysql.sh
+++ bootstrap-mysql.sh
@@ -5,16 +5,21 @@
 # DB_NAME=<compose>
 # DB_CXN_URL=<jdbc:mysql://bluemix-sandbox-dal-9-portal.6.dblayer.com:12121?createDatabaseIfNotExist=true>
 
-# assumes "script_params": ["DB_USER_NAME", "DB_PWD", "DB_NAME", "DB_CXN_URL-HOST" , "DB_CXN_URL-PORT"]
+# assumes "script_params": ["DB_USER_NAME", "DB_PWD", "DB_NAME", "DB_CXN_URL_HOST" , "DB_CXN_URL_PORT"]
 
 DB_USER_NAME=$1
 DB_PWD=$2
 DB_NAME=$3
-DB_CXN_URL="jdbc:mysql://"$4":"$5"?createDatabaseIfNotExist=true"
+DB_CXN_URL_HOST=$4
+DB_CXN_URL_PORT=$5
 
-echo "MySQL User: "$DB_USER_NAME
-echo "MySQL DB Name: "$DB_NAME
-echo "MySQL URL: "$DB_CXN_URL
+DB_CXN_URL="jdbc:mysql://"$DB_CXN_URL_HOST":"$DB_CXN_URL_PORT"/$DB_NAME?createDatabaseIfNotExist=true"
+
+echo "MySQL User: $DB_USER_NAME"
+echo "MySQL DB Name: $DB_NAME"
+echo "MySQL HOST: $DB_CXN_URL_HOST"
+echo "MySQL PORT: $DB_CXN_URL_PORT"
+echo "MySQL URL: $DB_CXN_URL"
 
 echo "NODE_TYPE: "$NODE_TYPE1
 
@@ -32,7 +37,7 @@ then
     echo "javax.jdo.option.ConnectionUserName = $DB_USER_NAME"
     /var/lib/ambari-server/resources/scripts/configs.sh -u $AMBARI_USER -p $AMBARI_PASSWORD -port $AMBARI_PORT -s set $AMBARI_HOST  $CLUSTER_NAME hive-site "javax.jdo.option.ConnectionUserName" $DB_USER_NAME
 
-    echo "javax.jdo.option.ConnectionPassword = $DB_PWD"
+    # echo "javax.jdo.option.ConnectionPassword = $DB_PWD"
     /var/lib/ambari-server/resources/scripts/configs.sh -u $AMBARI_USER -p $AMBARI_PASSWORD -port $AMBARI_PORT -s set $AMBARI_HOST  $CLUSTER_NAME hive-site "javax.jdo.option.ConnectionPassword" $DB_PWD
 
     echo "ambari.hive.db.schema.name = $DB_NAME"
@@ -44,6 +49,6 @@ then
 
     curl -v --user $AMBARI_USER:$AMBARI_PASSWORD -H "X-Requested-By: ambari" -i -X PUT -d '{"RequestInfo": {"context": "Start All Services via REST"}, "ServiceInfo": {"state":"STARTED"}}' https://$AMBARI_HOST:$AMBARI_PORT/api/v1/clusters/$CLUSTER_NAME/services
     sleep 700
-    
+
     echo "Completed customization"
 fi
